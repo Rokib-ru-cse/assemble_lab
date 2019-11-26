07384332d8964508b8cd5cb4edb4b5cb6faa7eda
Rokib Ahmed <37208161+Rokib-ru-cse@users.noreply.github.com>
2019-11-25 22:52:35 -0800

    Update stringReverse.asm

--- stringReverse.asm
+++ stringReverse.asm
@@ -30,10 +30,10 @@ EXIT:
 DEC SI
          
 MOV AH, 2
-MOV DL, 0DH
+MOV DL, 0DH ;enter new line
 INT 21H
 MOV AH, 2
-MOV DL, 0AH
+MOV DL, 0AH ; move cursour to the first
 INT 21H
          
 NEXT2:
